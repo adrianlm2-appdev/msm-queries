3018a78c3f0a5701835b70c2b229fb0d4f23b322
adrianlm2 <adrianl0@chicagobooth.edu>
2022-02-09 02:10:38 +0000

    eldest and youngest

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,7 +2,15 @@ class DirectorsController < ApplicationController
 
   def index 
     @list_of_directors = Director.all
-    render ({ :template => "director_templates/index.html.erb"})
+    render ({ :template => "director_templates/index.html.erb" })
   end 
+def eldest 
 
+  render ({ :template => "director_templates/eldest.html.erb" })
+end
+
+  def youngest 
+
+    render ({ :template => "director_templates/youngest.html.erb" })
+end 
 end 
